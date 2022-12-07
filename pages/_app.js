588b1917b1a2cedcbfbf494c1e6441d520e52d00
roshan-d02 <89829971+roshan-d02@users.noreply.github.com>
2022-12-08 00:56:02 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={5}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                "https://goerli.infura.io/v3/99a3fc6eba79419794820ef2a86082dc",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default MyApp;
